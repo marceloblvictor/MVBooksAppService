6da7d42a9d04aa9d437acf0dec1d1437dc435410
Marcelo Victor <marceloblvictor@gmail.com>
2025-05-12 21:11:17 +0100

    configure azure blob storage

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using MVBooksAppService;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,37 @@ var cosmosKey = builder.Configuration["CosmosDb:Key"];
 var cosmosDatabaseName = builder.Configuration["CosmosDb:DatabaseName"];
 
 builder.Services.AddAzureCosmosDb(cosmosConnectionString, cosmosKey, cosmosDatabaseName);
+
+var blobServiceClient = new BlobServiceClient(
+        new Uri("https://<storage-account-name>.blob.core.windows.net"),
+        new DefaultAzureCredential());
+
+string containerName = "quickstartblobs" + Guid.NewGuid().ToString();
+
+// Create the container and return a container client object
+BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+
+// Create a local file in the ./data/ directory for uploading and downloading
+string localPath = "data";
+Directory.CreateDirectory(localPath);
+string fileName = "quickstart" + Guid.NewGuid().ToString() + ".txt";
+string localFilePath = Path.Combine(localPath, fileName);
+
+// Write text to the file
+await File.WriteAllTextAsync(localFilePath, "Hello, World!");
+
+// Get a reference to a blob
+BlobClient blobClient = containerClient.GetBlobClient(fileName);
+await blobClient.SetMetadataAsync(new Dictionary<string, string>
+{
+    { "Resolution", "1920x1080" },
+    { "ColorProfile", "sRGB" }
+});
+
+Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+// Upload data from the local file, overwrite the blob if it already exists
+await blobClient.UploadAsync(localFilePath, true);
 
 
 var app = builder.Build();
@@ -32,10 +66,5 @@ app.MapControllers();
 app.Run();
 
 
-// set metadata to blob 
 
-//await blobClient.SetMetadataAsync(new Dictionary<string, string>
-//{
-//    { "Resolution", "1920x1080" },
-//    { "ColorProfile", "sRGB" }
-//});
+
